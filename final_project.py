b1af92aa104b1d7d4a3e8b7b5fe32ad9071e6367
IharSt <dsw51769@student.dsw.edu.pl>
2024-05-08 09:56:27 +0200

    add loading_json

--- final_project.py
+++ final_project.py
@@ -13,6 +13,22 @@ def main():
     args = parse_arguments()
     input_file = args.input_file
     output_file = args.output_file
+    if os.path.splitext(input_file)[1] == ".json":
+        obj = loading_json(input_file)
+        
+def loading_json(input_file):
+    for file in files:
+        if file == input_file:
+            with open(file, "r") as file_js:
+                file_content = file_js.read()
+                try:
+                    json_obj = json.loads(file_content)
+                    return json_obj
+                except:
+                    print("json is not written correctly ")
+                    sys.exit(1)
+    print("No such file")
+    sys.exit(1)
      
 
 if __name__ == "__main__":
